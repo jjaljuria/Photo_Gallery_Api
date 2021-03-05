5d25fa473b6d02bc13273ce4ccc25f070f5709b5
José Aljuria <josejavieral13@gmail.com>
2021-03-05 05:49:17 -0400

    problemas types bcrypt y así

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import config from './config';
+// import bcrypt from 'bcryptjs';
+// import session from 'express-session';
 
 import photoRouter from './routes/photo.router';
 
@@ -15,5 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/photo', photoRouter);
+// app.get('/', async (req, res)=>{
+// 	const salt = await bcrypt.genSalt(10);
+// 	const hash = bcrypt.hash('hola mundo', salt);
+// 	res.send(hash);
+// })
 
 export default app;
--- api/src/controllers/photo.controller.ts
+++ api/src/controllers/photo.controller.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from "express";
 import User from '../models/User';
+import bcryptjs from 'bcryptjs';
+
+
 export const getPhotos: RequestHandler = async (req, res) =>{
 	const photos = await User
 }
