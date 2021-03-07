f99e1196c802870a74abbb26d214f2a559dec8c0
José Aljuria <josejavieral13@gmail.com>
2021-03-06 21:48:10 -0400

    algo

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,13 +10,17 @@ import userRouter from './routes/user.router';
 
 const app = express();
 
+// Settings
 app.set('port', config.PORT);
 
+
+// Middlewares
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// Routes
 app.use('/photos', photoRouter);
 app.use('/user', userRouter);
 // app.get('/', async (req, res)=>{
