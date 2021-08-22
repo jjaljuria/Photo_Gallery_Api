2676a6684f30d567afe97d4243893cb546ba28c8
José Aljuria <josejavieral13@gmail.com>
2021-08-21 21:02:46 -0400

    revision database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import app from './app';
 import './database';
 
-
+console.log('databases_URL', process.env.DATABASE_URL);
+console.log('databases_URL', process.env.MONGO_URI);
+console.log('databases_URL', process.env.MONGO_LOCALHOST);
 app.listen(app.get('port'), () => {
 	console.log('Server on port ', app.get('port'));
 })
