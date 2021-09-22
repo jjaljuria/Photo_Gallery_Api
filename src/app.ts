4fd47fbe47dd1d0fa5f62719ab8f4d675ff6ead9
José Aljuria <josejavieral13@gmail.com>
2021-09-22 13:59:46 -0400

    add trusty proxy

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('trust proxy', true);
 app.use(session({
 	secret: 'mysecretapp',
 	resave: true,
