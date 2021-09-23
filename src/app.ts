bd8b3be8e782da1310b5ff42ede9a93c2436644b
José Aljuria <josejavieral13@gmail.com>
2021-09-23 10:45:47 -0400

    add package connect mongo for solution express-session store fail

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import config from './config';
 import session from 'express-session';
 import passport from 'passport';
 import path from 'path';
-
+import MongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
 import photoRouter from './routes/photo.router';
 import userRouter from './routes/user.router';
+
 
 // Inicializations
 const app = express();
@@ -31,12 +33,17 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 app.use(session({
 	secret: 'mysecretapp',
 	resave: true,
 	saveUninitialized: true,
+	store: MongoStore.create({
+		mongoUrl: `${!config.DEBUG ? config.MONGO_URI : config.MONGO_LOCALHOST}`,
+		dbName: 'photo_gallery'
+	})
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
